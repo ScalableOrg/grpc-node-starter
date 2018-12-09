b966e86cc24bc87c7d9218352928abe82a46d6b0
Adebayo Opesanya <bayoopesanya@gmail.com>
2018-12-09 02:33:04 +0100

    client connection to grpc

---
+++ src/client/includes/proto.ts
@@ -0,0 +1,11 @@
+const caller = require("grpc-caller");
+
+const { PROTO_LOCATION } = process.env;
+console.log(PROTO_LOCATION)
+export const operationsRPC = caller(
+`127.0.0.1:${process.env.GRPC_CLIENT_PORT}`,
+PROTO_LOCATION,
+'Arithmetic',
+);
+
+console.log(`Listening to ARITHMETIC RPC at localhost: ${process.env.GRPC_CLIENT_PORT}`);
